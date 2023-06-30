814f669e0b5dffdfb66651dc0a961fe41a09c3c4
JlordS32 <jlordsty128@gmail.com>
2023-06-30 21:15:16 +1000

    initial commit

--- src/components/home/Todo.jsx
+++ src/components/home/Todo.jsx
@@ -1,92 +1,87 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Todobox from './TodoBox';
-
 import '../../styles/todo.modules.css';
 
 const Todo = () => {
-   const [newTodo, setNewTodo] = useState('');
-   const [newTodoTitle, setNewTodoTitle] = useState('');
-   const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+  const [newTodoTitle, setNewTodoTitle] = useState('');
+  
+  const [todos, setTodos] = useState([]);
+  const [onBlur, setOnBlur] = useState(true);
 
-   const [onBlurText, setOnBlurText] = useState(false);
+  const handleInputChange = (e) => {
+    switch (e.target.name) {
+      case 'title':
+        setNewTodoTitle(e.target.value);
+        break;
+      case 'todo-content':
+        setNewTodo(e.target.value);
+        break;
+      default:
+        return;
+    }
+  };
 
-   const textareaRef = useRef();
+  const handleBlur = (e) => {
+    if (!e.currentTarget.contains(e.relatedTarget)) {
+      setOnBlur(true);
+    }
+  };
 
-   const handleInputChange = (e) => {
-      switch (e.target.name) {
-         case 'title':
-         setNewTodoTitle(e.target.value);
-         break;
-         case 'todo-content':
-         setNewTodo(e.target.value);
-         break;
-         default:
-         return;
+  const handleFocus = (e) => {
+    if (!e.currentTarget.contains(e.relatedTarget)) {
+      setOnBlur(false);
+    }
+  };
+
+  useEffect(() => {
+    if (onBlur) {
+      if (newTodo.trim() !== '') {
+        const title = newTodoTitle.trim() !== '' ? newTodoTitle : 'No Title';
+        const content = newTodo;
+
+        setTodos((todos) => [...todos, { title, content }]);
+        setNewTodo('');
+        setNewTodoTitle('');
       }
-   };
+    }
+  }, [onBlur]);
 
-   const handleBlur = (e) => {
-      setOnBlurText(false);
-
-      console.log(`I'm leaving ${e.target.name} at the moment hehe...`);
-   };
-
-   const handleFocus = (e) => {
-      setOnBlurText(true);
-
-      console.log(`I'm touching ${e.target.name} at the moment hehe...`);
-   }
-
-   useEffect(() => {
-      // Dynamically adjust the textarea height based on content
-      textareaRef.current.style.height = 'auto';
-      textareaRef.current.style.height = `${textareaRef.current.scrollHeight + 150}px`;
-   }, [newTodo]);
-
-   useEffect(() => {
-      if (onBlurText) {
-         if (newTodo.trim() !== '') {
-            const title = newTodoTitle.trim() !== '' ? newTodoTitle : 'No Title';
-            const content = newTodo;
-   
-            setTodos((todos) => [...todos, { title, content }]);
-            setNewTodo('');
-         }
-      }
-   }, [onBlurText]);
-
-   return (
-      <>
-         <div className='newtodo-container'>
-         <div className='newtodo-box'>
+  return (
+    <>
+      <div
+        className='newtodo-container'
+        tabIndex='0'
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      >
+        <div className='newtodo-box'>
+          {!onBlur && (
             <input
-               type='text'
-               value={newTodoTitle}
-               onChange={handleInputChange}
-               onBlur={handleBlur}
-               onFocus={handleFocus}
-               name='title'
-               className='newtodo-title'
-               placeholder='Enter title...'
+              type='text'
+              value={newTodoTitle}
+              onChange={handleInputChange}
+              name='title'
+              className='newtodo-title'
+              placeholder='Enter title...'
             />
-            <textarea
-               value={newTodo}
-               ref={textareaRef}
-               onChange={handleInputChange}
-               name='todo-content'
-               className='newtodo-content'
-               onFocusCapture={handleFocus}
-               onBlur={handleBlur}
-            />
-         </div>
-         </div>
-         <div className='todos-wrapper'>
-         {todos.map((todo, index) => (
-            <Todobox key={index} todo={todo} />
-         ))}
-         </div>
-      </>
-   );
+          )}
+          <textarea
+            value={newTodo}
+            placeholder='Type a note...'
+            onChange={handleInputChange}
+            name='todo-content'
+            className='newtodo-content'
+          />
+        </div>
+      </div>
+      <div className='todos-wrapper'>
+        {todos.map((todo, index) => (
+          <Todobox key={index} todo={todo} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Todo;
