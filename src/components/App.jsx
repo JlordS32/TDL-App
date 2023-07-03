4268a7876a021f287f96723003292c008f275a6b
JlordS32 <jlordsty128@gmail.com>
2023-07-03 18:20:44 +1000

    updated css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import '../styles/app.modules.css'
 
 import NewTodo from './home/Todo';
+import Nav from './header/Nav';
 
 
 const App = () => {
 
    return (
       <div className='app'>
+         <Nav />
          <NewTodo />
       </div>
    )
---
+++ src/components/header/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../../styles/nav.modules.css';
+
+const Nav = () => {
+   return (
+      <div className='nav-container'>
+         Just some TO DO LIST APP...
+      </div>
+   )
+}  
+
+export default Nav;
