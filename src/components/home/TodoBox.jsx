a41159cb5465ac20660be5cbb65a9cb9beefe355
JlordS32 <jlordsty128@gmail.com>
2023-07-06 00:52:51 +1000

    updated

--- src/components/home/TodoBox.jsx
+++ src/components/home/TodoBox.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, {
+   useContext
+} from 'react';
 import '../../styles/todo.modules.css';
+import { TodoContext } from './Todo';
 
 import DeleteIcon from '../../assets/icons/DeleteIcon';
 import MoreIcon from '../../assets/icons/MoreIcon';
 
 const Todobox = ({todo}) => {
 
+   // Using context to access state from Todo component
+   // const { setTodos, todos } = useContext(TodoContext);
+
    const {
       title,
-      content
+      content,
+      id
    } = todo;
 
-   const handleClick = (e) => {
-      alert('Deleted!');
-      console.log(e.target.name);
+   const handleDelete = () => {
+      
    }
 
    return (
@@ -24,8 +30,8 @@ const Todobox = ({todo}) => {
          <div className='todo-content'>
             {content}
          </div>
-         <div className='todo-icons' onClick={handleClick}>
-            <div className='todo-icon-item delete-btn'> 
+         <div className='todo-icons'>
+            <div className='todo-icon-item delete-btn' onClick={handleDelete}> 
                <DeleteIcon color='white'/>
             </div>
             <div className='todo-icon-item more-btn'> 
