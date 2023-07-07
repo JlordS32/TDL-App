dfc1c5bccb3ca762141bfa58d9f08bb146b0f538
JlordS32 <jlordsty128@gmail.com>
2023-07-07 23:49:08 +1000

    created modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,13 @@ import '../styles/app.modules.css'
 
 import { 
    createContext, 
-   useState,
-   useContext
+   useState
 } from 'react';
 
 import Todo from './home/Todo';
 import Header from './header/Header';
 import Nav from './header/Nav';
 import Modal from './dialog/Modal';
-import { TodoBoxContext } from './home/TodoBox';
 
 export const ModalContext = createContext();
 
@@ -18,31 +16,33 @@ const App = () => {
 
 
    const [modalOpen, setModalOpen] = useState(false);
-   const { id } = useContext(TodoBoxContext);
+   const [id, setId] = useState('');
 
    // Toggle modal functions 
    const close = () => setModalOpen(false);
-   const open = () => setModalOpen(true);
+   const open = () => {
+      setModalOpen(true);
+      console.log(id);
+   };
 
    return (
       <ModalContext.Provider 
          value={{
             open, 
             close, 
-            modalOpen
+            modalOpen,
+            setId
          }}>
          <div className='app'>
             <Nav />
             <Header />
             <Todo />
-
-            // If list is clicked, modal is opened
             {modalOpen && 
                <Modal   
                   modalOpen={modalOpen} 
                   handleClose={close}
                   id={id}
-               />}
+            />}
          </div>
       </ModalContext.Provider>
    )
