8297695f2024be77b02a2d932b5a560a53ecaa49
JlordS32 <jlordsty128@gmail.com>
2023-07-09 23:45:13 +1000

    installed and added redux

---
+++ src/components/store/TodoReducer.jsx
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialStateValue = {
+   id: '',
+   title: '', 
+   content: '',
+   complete: false,
+}
+
+export const todoSlice = createSlice({
+   name: 'todoReducer',
+   initialState: {
+      value: initialStateValue
+   },
+   reducers: {
+      updateTodo: (state, action) => {
+         state.value = action.payload;
+      }
+   }
+});
+
+export const { updateTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
