8090e0d1e9c60fd3cf711d3cccaa83a74a5d8b7b
JlordS32 <jlordsty128@gmail.com>
2023-07-15 15:29:53 +1000

    added svg

--- src/components/home/Search.jsx
+++ src/components/home/Search.jsx
@@ -1,7 +1,34 @@
 import '../../styles/todo.modules.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateTodo } from '../store/TodoReducer';
 
 const Search = () => {
+
+   const todos = useSelector(state => state.todoReducer.value);
+   const dispatch = useDispatch();
+
+   const [searchQuery, setSearchQuery] = useState('');
+
+   const handleChange = (e) => {
+      setSearchQuery(e.target.value);
+   };
+
+   const filteredTodo = todos.filter(todo => {
+      const filteredContent = todo.content.toLowerCase().includes(searchQuery.toLowerCase());
+
+      const filteredTitle = todo.title.toLowerCase().includes(searchQuery.toLowerCase());
+
+      if (filteredContent || filteredTitle) {
+         return todo;
+      }
+   });
+
+   useEffect(() => {
+      dispatch(updateTodo(filteredTodo));
+   }, [searchQuery]);
+   
+
    return (
       <div className='search-container'>
          <input 
@@ -9,6 +36,7 @@ const Search = () => {
             name='search-bar'
             className='search-bar'
             placeholder='Search'
+            onChange={handleChange}
          />
       </div>
    )
