9c7c002a6328d3d5d5b27f16f8e76121c9f3484a
JlordS32 <jlordsty128@gmail.com>
2023-07-17 20:52:36 +1000

    commit of the day

--- src/components/header/Nav.jsx
+++ src/components/header/Nav.jsx
@@ -1,20 +1,47 @@
-import '../../styles/nav.modules.css';
-import LabelIcon from '../../assets/icons/LabelIcon';
+import "../../styles/nav.modules.css";
+import LabelIcon from "../../assets/icons/LabelIcon";
+import { useEffect, useState } from "react";
+import NavItemTitles from "./NavItemTitles";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 const Nav = () => {
+  const [mouseEnter, setMouseEnter] = useState(false);
+  const handleMouseEnter = () => {
+    setMouseEnter(true);
+  };
+
+  const handleMouseLeave = () => {
+    setMouseEnter(true);
+  };
+
   return (
-    <div className='nav-container'>
-      <div className='nav-icons'>
-        <LabelIcon width='25'/>
+    <div className="nav-container">
+      <div
+        className="nav-icons-container"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <div className="">
+          <div className="nav-icons">
+            <LabelIcon width="25" />
+          </div>
+          <div className="nav-icons">
+            <LabelIcon width="25" />
+          </div>
+          <div className="nav-icons">
+            <LabelIcon width="25" />
+          </div>
+        </div>
       </div>
-      <div className='nav-icons'>
-        <LabelIcon width='25'/>
-      </div>
-      <div className='nav-icons'>
-        <LabelIcon width='25'/>
-      </div>
+
+      {mouseEnter && (
+         <NavItemTitles 
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+         />
+      )} 
     </div>
   );
-}
+};
 
 export default Nav;
