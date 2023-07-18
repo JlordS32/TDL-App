e78077615adbce1396e7e714bd06980cbd5a7ed3
JlordS32 <jlordsty128@gmail.com>
2023-07-18 20:38:51 +1000

    updating dialog

--- src/components/header/NavItemTitles.jsx
+++ src/components/header/NavItemTitles.jsx
@@ -1,16 +1,51 @@
-import '../../styles/nav.modules.css';
+import { useEffect, useState } from "react";
+import "../../styles/nav.modules.css";
 
 const NavItemTitles = ({ onMouseEnter, onMouseLeave }) => {
+  const [mouseEnter, setMouseEnter] = useState(false);
+
+  const handleMouseEnter = () => {
+    setMouseEnter(true);
+  };
+
+  const handleMouseLeave = () => {
+    setMouseEnter(false);
+  };
+
+  const iconTitle = {
+    group: "Group",
+    test: "Test",
+  };
+
   return (
     <div
-      className='icon-title'
+      className="icon-title"
       onMouseLeave={onMouseLeave}
       onMouseEnter={onMouseEnter}
     >
-      <div className='icon-title-wrapper'>
-        <div>Folder</div>
-        <div>Test</div>
-        <div>Test</div>
+      <div className="icon-title-wrapper">
+        <div
+          className="nav-icon-title todo-group"
+          data-name='React'
+          onMouseLeave={handleMouseLeave}
+          onMouseEnter={handleMouseEnter}
+        >
+          {iconTitle.group}
+        </div>
+        <div
+          className="nav-icon-title"
+          onMouseLeave={handleMouseLeave}
+          onMouseEnter={handleMouseEnter}
+        >
+          {iconTitle.test}
+        </div>
+        <div
+          className="nav-icon-title"
+          onMouseLeave={handleMouseLeave}
+          onMouseEnter={handleMouseEnter}
+        >
+          {iconTitle.test}
+        </div>
       </div>
     </div>
   );
