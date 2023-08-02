aeb8ec5c3df6cd543df722c626991b8b44bb4a43
JlordS32 <jlordsty128@gmail.com>
2023-08-02 23:14:25 +1000

    working on nav while dying inside

--- src/components/navigation/NavItems.jsx
+++ src/components/navigation/NavItems.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import XIcon from '../../assets/icons/XIcon';
+import Groups from './navItems/Groups';
+import Test from './navItems/Test';
 
 const NavItems = ({ toggleItem, item, close }) => {
+	const componentMap = {
+		groups: <Groups />,
+		test: <Test />,
+	};
+
+	const content = componentMap[item.toLowerCase()] || (
+		<div>Page Not Found!</div>
+	);
+
 	return (
-		<div className={`nav-items-container ${toggleItem ? 'show' : 'hide'}`}>
-			<div onClick={close} className='close-item'>
-				<XIcon width='30' height='30'/>
+		<div
+			className={`
+			nav-items-container 
+			${toggleItem ? 'show' : 'hide'}`}
+		>
+			<div
+				onClick={close}
+				className='close-item'
+			>
+				<XIcon
+					width='30'
+					height='30'
+				/>
 			</div>
-			{item}
+			{content}
 		</div>
 	);
 };
---
+++ src/components/navigation/navItems/Groups.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Groups = () => {
+	return (
+      <h3>Groups</h3>
+   );
+}
+
+export default Groups;
